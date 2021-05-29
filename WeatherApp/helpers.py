d8cec0dad152d3a9e566fabe6e49d35715598361
abullity <abullity@gmail.com>
2021-05-29 10:40:45 +0400

    fixed local time processing for each city

--- WeatherApp/helpers.py
+++ WeatherApp/helpers.py
@@ -2,7 +2,7 @@ import os
 import json
 import random
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 from WeatherApp.models import City
     
 
@@ -16,14 +16,13 @@ def get_temp_verbal(temp):
 	return temp_verbal
 	
 	
-def get_image(epoch, shift):
-	local_epoch = epoch - shift
-	local_hours = datetime.fromtimestamp(local_epoch).time().hour
+def get_image(timezone_shift):
+	local_hours = (datetime.utcnow() + timedelta(seconds=timezone_shift)).hour
 	if local_hours < 7:
 		image = 'night'
-	elif local_hours <= 9:
+	elif local_hours < 9:
 		image = 'evening-morning'
-	elif local_hours <= 18:
+	elif local_hours < 18:
 		image = 'day'
 	elif local_hours < 22:
 		image = 'evening-morning'
@@ -38,19 +37,18 @@ def get_weather_data(city_name):
 	# an API key on https://openweathermap.org
 	# Once you get your key, change the following variable like so:
 	# API_KEY = 'some_random_character_sequence'
-    API_KEY = os.environ['OWAPI_KEY']
-    # ^
-    # | 
-    # Change that variable
-    data = requests.get(f"""http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}""")
-    return json.loads(data.content)
+	API_KEY = os.environ['OWAPI_KEY']
+	# ^
+	# | 
+	# Change that variable
+	data = requests.get(f"""http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}""")
+	return json.loads(data.content)
 
 	
 def generate_city(weather_data):
 	name = weather_data['name']
-	# openweather api sends epoch as 'dt' and shift in user's timezone in 
-	# seconds as 'timezone'
-	image = get_image(weather_data['dt'], weather_data['timezone'])
+	# openweather api sends shift in user's timezone in seconds as 'timezone'
+	image = get_image(weather_data['timezone'])
 	temp = int(weather_data['main']['temp'] - 273.15)
 	temp_verbal = get_temp_verbal(temp)
 	return City(name=name, temp=temp, temp_verbal=temp_verbal, image=image)
@@ -61,4 +59,6 @@ def update_city(city):
 	temp = int(weather_data['main']['temp'] - 273.15)
 	city.temp = temp
 	city.temp_verbal = get_temp_verbal(temp)
-	city.image = get_image(weather_data['dt'], weather_data['timezone'])
+	city.image = get_image(weather_data['timezone'])
+
+
